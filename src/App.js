bd0222074bc682529e242b2fae7c9d8a1ccb619b
netanel <netanelmalako@gmail.com>
2022-03-01 23:05:16 +0200

    ddd

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { HashRouter as Router ,Routes , Route , Link } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     worksss
+    </div>
+  );
+}
+
+export default App;
